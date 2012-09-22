4834a53b779d3699d06b36187cd6a0986a061bf0
Austin Ogilvie <austinogilvie@gmail.com>
2012-09-21 22:28:14 -0400

    setting content type and http status in headers

--- naics.py
+++ naics.py
@@ -1,18 +1,24 @@
 import json
 import pprint as pp
 from pymongo import Connection
-db = Connection()['industries']
 
+naics_db = Connection()['industries']
 
-def fetch_naics():
-    naics = []
-    for n in db.naics_2007.find():
-        del n['_id']
-        naics.append(n)
-    return naics
+class naics(object):
+    def __init__(self, db=naics_db):
+        self.db = db
 
-def fetch_one(code):
-    doc = db.naics_2007.find_one({"2007_naics_us_code":str(code)})
-    if "_id" in doc:
-        del doc["_id"]
-    return doc
+    def find_all(self):
+        codes = []
+        for n in self.db.naics_2007.find():
+            if "_id" in n:
+                del n['_id']
+            codes.append(n)
+        return codes
+
+    def find(self, naics_code):
+        doc = self.db.naics_2007.find_one({"2007_naics_us_code":str(naics_code)})
+        code = doc if doc else {}
+        if "_id" in code:
+            del code["_id"]
+        return code
--- server.py
+++ server.py
@@ -4,16 +4,21 @@ import json, os
 from naics import *
 
 app = Flask(__name__)
+Naics = naics()
 
 @app.route("/naics/all", methods=['GET'])
 def naics():
-    return Response(json.dumps(fetch_naics()), mimetype="application/json")
+    return Response(json.dumps(Naics.find_all()), mimetype="application/json")
 
 @app.route('/naics/<int:code>', methods=['GET'])
 def find_one(code):
-    return Response(json.dumps(fetch_one(code)), mimetype="application/json")
+    docs = Naics.find(code)
+    res = Response(json.dumps(docs), status=200, mimetype='application/json')
+    res.headers['Title'] = 'NAICS 2007 Industry Codes'
+    return res
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+    #app.run(debug=True)
 
